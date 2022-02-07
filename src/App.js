31cf4fc99cd2fc01deb6757b4d5b5b468f22c3c1
nixigg86 <dev.py86@gmail.com>
2022-02-07 01:18:36 -0800

    First commit, logic is done for rating service, add a review, delete a review. About page added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackForm from "./components/FeedbackForm";
+import FeedbackStats from "./components/FeedbackStats";
+import AboutPage from "./pages/AboutPage";
 
-function App() {
+const App = () => {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route exact path="/about" element={<AboutPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,49 @@
+import { motion, AnimatePresence } from "framer-motion";
+import React from "react";
+import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
+
+const FeedbackList = ({ feedback, handleDelete }) => {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
+  }
+
+  return (
+    <div className="feedback-list">
+      <AnimatePresence>
+        {feedback.map((item) => {
+          return (
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem
+                key={item.id}
+                item={item}
+                handleDelete={handleDelete}
+              />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
+    </div>
+  );
+
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => {
+  //       return (
+  //         <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //       );
+  //     })}
+  //   </div>
+  // );
+};
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.array,
+};
+
+export default FeedbackList;
